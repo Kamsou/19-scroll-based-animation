e1a51e05ed0c50a368624deab95a194eb5149a4a
Camille Coutens <camille.coutens@gmail.com>
2024-01-01 18:41:44 +0100

    routing

---
+++ src/routing.js
@@ -0,0 +1,26 @@
+// Obtenez le chemin de l'URL actuelle
+const currentURL = window.location.pathname;
+
+// Fonction pour charger un script dynamiquement
+function loadScript(scriptURL) {
+  const script = document.createElement("script");
+  script.src = scriptURL;
+  script.type = "module";
+  document.body.appendChild(script);
+}
+
+if (currentURL === "/sphere") {
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    section.style.display = "none";
+  });
+  loadScript("sphere-test.js");
+} else if (currentURL === "/sphere-2") {
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    section.style.display = "none";
+  });
+  loadScript("sphere-test-3.js");
+} else {
+  loadScript("script_noel.js");
+}
